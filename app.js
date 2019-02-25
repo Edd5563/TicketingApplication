8a50f3462f7cb320ae20e2f9e5904e41f358fd31
edwin rivera <techedwin@aol.com>
2019-02-25 00:23:36 -0500

    adding files ive already worked on

---
+++ app.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+
+    $('.incomingCall').hide();// hiding content for now
+    $('.customerForm').hide();// hiding content for now
+
+
+
+    $(".clientBtn").click(function () {
+        
+        $('.incomingCall').hide();
+        $('.customerForm').show();
+    });
+    
+    $(".messageBtn").click(function () { 
+        
+        $('.customerForm').hide();
+        $('.incomingCall').show();
+    });
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); //document.ready close
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for later implementation
+
+//------------------- ^ ||Seperation barrier|| v ---------------------    
+// Ajax call to weather api to grab local weather form form adn display.
+//May need a node server to make work
+
+
+   // $(".zipBtn").click(function () {
+    //     event.preventDefault();
+
+
+    //     var apiKey = 'd2b1261b901d14b915dc95d807622fc2'
+    //     var zip = $('#zipInput').val();
+    //     var url = `https://samples.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}`
+
+
+
+
+    //     //Attempt1
+    //     $.get(url, function(data) {
+    //         console.log(data)
+    //     });
+
+
+    //     //Attempt2
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.onreadystatechange = function () {
+    //         if (xhr.readyState === 4) {
+    //             if (xhr.status === 200) {
+    //                 console.log(xhr.responseText)
+    //             } else {
+    //                 alert(xhr.statusText);
+    //             }
+    //         } 
+    //     }
+    //     xhr.open('GET', "url");
+    //     xhr.send();
+
+
+    // });
+//------------------- ^ ||Seperation barrier|| v ---------------------    
+
+//end of ideas
