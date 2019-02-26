b783466cb43027e3c0ae0db8bb3f4a3973fb779e
edwin rivera <techedwin@aol.com>
2019-02-26 17:30:56 -0500

    added more features, issues with weather api

--- app.js
+++ app.js
@@ -39,14 +39,107 @@ $(document).ready(function () {
     });
 
 
+//------------------------------------------------------
+    function getWeather() {
+        var apiKey = 'd2b1261b901d14b915dc95d807622fc2'
+        var zip = $('#zipInput').val();
+        var url = `https://samples.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}`
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    var weather = JSON.parse(xhr.responseText)
+                } else {
+                    alert(xhr.statusText);
+                }
+            }
+        }
+        xhr.open('GET', "url");
+        xhr.send();
+    }
+//------------------------------------------------------
+
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    var customers = JSON.parse(xhr.responseText);
+                    
+                    //console.log(customers)
+                    for (let i = 0; i < customers.length; i++) {
+                        
+                        
+                        var customerHTML = `
+                        <div class="card" style="width: 15rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">${customers[i].fullName}</h5>
+                                <p class="card-text">${customers[i].address}</p>
+                                <p class="card-text">${customers[i].secondAddress}</p>
+                                <p class="card-text">${customers[i].companyName}</p>
+                                <p class="card-text">${customers[i].phoneNumber}</p>
+                                <p class="card-text">${customers[i].city}, ${customers[i].state}</p>
+                                <p class="card-text">${customers[i].zip}</p>
+                            </div>
+                        </div>
+                        <br>`;
+                        $(".customerInfo").append(customerHTML);
+                    }
+
+
+
+                } else {
+                    alert(xhr.statusText);
+                }
+            } 
+        }
+        xhr.open('GET', "../customerData.json");
+        xhr.send();
+
+
+
+
+    //     //Attempt2
+    function getWeather() {
+        var apiKey = 'd2b1261b901d14b915dc95d807622fc2'
+        var zip = $('#zipInput').val();
+        var url = `https://samples.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}`
+        console.log(url);
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    var weather = JSON.parse(xhr.responseText)
+                } else {
+                    alert(xhr.statusText);
+                }
+            } 
+        }
+        xhr.open('GET', "url");
+        xhr.send();
+
+    }
+ 
+
+
+
+   $(".zipBtn").click(function () {
+        event.preventDefault();
+        getWeather();
+    });
+
+
+
+
+
+
+
+
 
 
 }); //document.ready close
-
-
-
-
-
 
 
 
@@ -80,6 +173,7 @@ $(document).ready(function () {
 
 
 
+
 //for later implementation
 
 //------------------- ^ ||Seperation barrier|| v ---------------------    
@@ -104,19 +198,7 @@ $(document).ready(function () {
     //     });
 
 
-    //     //Attempt2
-    //     var xhr = new XMLHttpRequest();
-    //     xhr.onreadystatechange = function () {
-    //         if (xhr.readyState === 4) {
-    //             if (xhr.status === 200) {
-    //                 console.log(xhr.responseText)
-    //             } else {
-    //                 alert(xhr.statusText);
-    //             }
-    //         } 
-    //     }
-    //     xhr.open('GET', "url");
-    //     xhr.send();
+
 
 
     // });
