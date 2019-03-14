f645a6fccc55c77cd2223287deff7bff6b97d818
edwin rivera <techedwin@aol.com>
2019-03-13 23:19:09 -0400

    Improved on some aspects of the program

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let mongoose = require('mongoose')
+
 let states = require('./assets/js/states');
+const custRegs = require('./models/regCustomers')
+
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+mongoose.Promise = global.Promise;
+
 let app = express();
+
+
+
+mongoose.connect('mongodb://localhost/customerDB', { useNewUrlParser: true })
+mongoose.connection.once('once', function () {
+    console.log('Connection has been Established')
+
+}).on('error', function (error) {
+    console.log('Connection error: ' + error)
+});
+
+
+
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
@@ -24,7 +43,8 @@ app.get('/ticketForm', function (req, res) {
 });
 
 app.post('/customerReg', urlencodedParser, function(req, res) {
-    console.log(req.body);
+    var customer = new custRegs(req.body)
+    customer.save();
 });
 
 const port = 3000
--- models/regCustomers.js
+++ models/regCustomers.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RegCustomerSchema = new Schema({
-	firstName: String,
-	lastName: String,
-	phone: Number,
+	fName: String,
+	lName: String,
+	telephone: String,
 	address: String,
+	addressTwo: String,
 	city: String,
 	state: String,
-	zip: Number,
-	companyName: String,
+	zip: String,
+	company: String,
 	email: String,
 });
 
 const CustRegs = mongoose.model('custregs', RegCustomerSchema);
-modules.export = CustRegs;
+module.exports = CustRegs;
