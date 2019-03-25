488d310ab4116e4476e54e3ac2f6c154a6f290c9
edwin rivera <techedwin@aol.com>
2019-03-24 20:12:53 -0400

    having issues with ejs limitations. Changing to Angular

--- app.js
+++ app.js
@@ -30,6 +30,16 @@ mongoose.connection.once('once', function () {
 });
 
 
+mongoose.connect('mongodb://localhost/messagesDB', { useNewUrlParser: true })
+mongoose.connection.once('once', function () {
+    console.log('Connection has been Established')
+
+}).on('error', function (error) {
+    console.log('Connection error: ' + error)
+});
+
+
+
 const port = process.env.port || 3000
 app.listen(port, function(){
     console.log('Now listening on port ' + port);
--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ let bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 const custRegs = require('../models/regCustomers')
 const userGroups = require('../models/users')
+const saveMessage = require('../models/messages')
 let states = require('../assets/js/states');
 
 
@@ -18,12 +19,12 @@ router.get('/', function (req, res) {
 router.post('/login', urlencodedParser, function (req, res) {
     var users = new userGroups(req.body)
     //console.log(users)
-    if (users.userName === "username" && users.password === "passeord") {
+    if (users.userName === '1325' && users.password === "1234") {
         res.render('customerReg', { states: states });
 
-        console.log(req.url)
+        // console.log(req.url)
     } else {
-        alert('Incorrect username or password')
+        res.render('login');
     }
 });
 //------------------------
@@ -32,22 +33,10 @@ router.post('/login', urlencodedParser, function (req, res) {
 
 
 
-
-
-
-
-
-
 router.get('/ticketForm', function (req, res) {
     res.render('ticketForm');
-    console.log(req.url)
+    // console.log(req.url)
 });
-
-
-
-
-
-
 
 
 
@@ -57,34 +46,33 @@ router.get('/ticketForm', function (req, res) {
 // making such a hude options list.
 router.get('/customerReg', function (req, res) {
     res.render('customerReg', { states: states });
-    console.log(req.url)
+    // console.log(req.url)
 });
 
 router.post('/customerReg', urlencodedParser, function (req, res) {
-    var customer = new custRegs(req.body)
+    let customer = new custRegs(req.body)
     console.log(customer)
     res.render('customerReg', { states: states });
-    console.log(req.url)
+    // console.log(req.url)
     // customer.save();
 });
 //------------------------
 
 
 
-
-
-
-
-
-
-
-
-
-
 router.get('/messages', function (req, res) {
     res.render('messages');
-    console.log(req.url)
+    // console.log(req.url)
 });
+
+
+router.post('/messages', urlencodedParser, function (req, res) {
+    let custMessage = new saveMessage(req.body)
+    console.log(custMessage)
+    res.render('messages', {data: custMessage})
+    
+});
+
 
 
 
