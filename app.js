ed0cac566f4f3fc4bacadffc0aa6642d5fc62566
edwin rivera <techedwin@aol.com>
2019-04-07 00:25:14 -0400

    file changes from errors

--- app.js
+++ app.js
@@ -8,18 +8,19 @@ let mongoose = require('mongoose')
 
 let app = express();
 
-app.use('/api', require('./routes/api'));
+app.use('/', require('./routes/api'));
 app.set('view engine', 'ejs');
 
 //Error handler
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
     console.log(err.message);
-    res.send({error:err.message});
+    res.send({ error: err.message });
 });
 
 mongoose.Promise = global.Promise;
 
-//mongodb://localhost/usersDB
+
+//mongoose.connect('mongodb://localhost/usersDB', { useNewUrlParser: true })
 mongoose.connect('mongodb://devtester:4devuse@ds061365.mlab.com:61365/heroku_sfr6qnmd', { useNewUrlParser: true })
 mongoose.connection.once('once', function () {
     console.log('Connection has been Established')
@@ -30,7 +31,9 @@ mongoose.connection.once('once', function () {
 
 
 
+
+
 const port = process.env.PORT || 5000
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('Now listening on port ' + port);
 });
